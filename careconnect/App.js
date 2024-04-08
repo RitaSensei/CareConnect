94e152aa98f74b0e0e75d114a99eb6e5dee0f633
Ghita <85077489+Ghita-L28@users.noreply.github.com>
2024-04-08 00:48:32 +0000

    set up expo app  and added firebase configuration

---
+++ careconnect/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>We are readyyyyyyy</Text>
+      <Text>This is actually fun</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ careconnect/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLRzR4uqoeO7Uc6pOA-3uTNx1PhxL-bb0",
+  authDomain: "careconnect-66481.firebaseapp.com",
+  projectId: "careconnect-66481",
+  storageBucket: "careconnect-66481.appspot.com",
+  messagingSenderId: "9285222114",
+  appId: "1:9285222114:web:631966364d7e7650d268bb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
